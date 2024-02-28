e847b815a514c34cc1288c1766df9102b907c43d
Lalit M <lalitm1004@gmail.com>
2024-02-28 18:50:20 +0530

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,236 @@
-import Image from "next/image";
+"use client";
+import { motion } from "framer-motion";
+import { Progress } from "@/components/ui/progress";
+import { Noto_Serif_Khojki } from "next/font/google";
+import { useEffect, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Skull } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+const noto = Noto_Serif_Khojki({subsets: ["latin"]});
 
 export default function Home() {
+  
+  const [input, setInput] = useState("");
+  const [weeksLived, setWeeksLived] = useState(0);
+  const [valid, setValid] = useState(null);
+  const [svgArray, setSvgArray] = useState(null);
+
+  const handleInput = (event) => {
+    
+    const checkValidity = (dateString) => {
+      const dateFormat = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+
+      if (!dateFormat.test(dateString)) {
+        return (valid == null) ? null : false; 
+      }
+      
+      let [, day, month, year] = dateString.match(dateFormat);
+  
+      day = parseInt(day, 10);
+      month = parseInt(month, 10);
+      year = parseInt(year, 10);
+      
+      if (month < 1 || month > 12) {
+        return (valid == null) ? null : false; 
+      }
+
+      if (day < 1 || day > 31) {
+        return (valid == null) ? null : false; 
+      }
+      
+      const today = new Date()
+      if (year > today.getFullYear()) {
+        return (valid == null) ? null : false; 
+      }
+      
+      if (year == today.getFullYear()) {
+        if (month > today.getMonth() + 1) {
+          return (valid == null) ? null : false; 
+        } else if (month == today.getMonth() + 1) {
+          if (day > today.getDate()) {
+            return (valid == null) ? null : false; 
+          } 
+        }
+      }
+
+      if (year < today.getFullYear() - 78) {
+        return (valid == null) ? null : false; 
+      }
+
+      const daysInMonth = new Date(year, month, 0).getDate();
+      if (day > daysInMonth) {
+        return (valid == null) ? null : false; 
+      }
+
+      return true;
+    } 
+
+    setValid(checkValidity(event.target.value));
+    setInput(event.target.value);
+  }
+
+  const handleClick = () => {
+    const weeksPassedSince = (dateString) => {
+      const [day, month, year] = dateString.split('/').map(part => parseInt(part, 10));
+      const inputDate = new Date(year, month - 1, day);
+      const differenceMs = Date.now() - inputDate.getTime();
+      const weeksPassed = Math.floor(differenceMs / (1000 * 60 * 60 * 24 * 7));
+      return weeksPassed;
+    }
+    setWeeksLived(weeksPassedSince(input));
+  }
+
+  useEffect(() => {
+    const BG = "#EBE7B8"
+    const FILL = "#000000"
+    const STROKE = "#000000"
+
+    const intialX = 55;
+    const initialY = 55;
+    const boxWidth = 25;
+    const gap = 10;
+    const moat = 50;
+
+    let x_offset = 0;
+    let y_offset = 0;
+
+    const rowsColored = Math.floor(weeksLived / 52);
+    const boxesColored = weeksLived - 52 * rowsColored;
+    const boxesBlank = 52 - boxesColored;
+
+    const localSvgArray = []
+
+    const getRectangle = (x, y, title, fill) => {
+      return (
+        <rect x={x} y={y} width={boxWidth} height={boxWidth} fill={(fill ? FILL : BG)} stroke={STROKE}><title>{title}</title></rect>
+      )
+    }
+
+    // Colored rows
+    for (let i = 0; i < rowsColored; i++) {
+      for (let j = 0; j < 52; j++) {
+        localSvgArray.push(getRectangle(
+          intialX + x_offset,
+          initialY + y_offset,
+          `Week ${i * 52 + j + 1}`,
+          true
+        ))
+        x_offset += boxWidth + gap;
+      }
+      if ((i + 1) % 10 == 0) {
+        y_offset += moat + gap;
+      } else {
+        y_offset += boxWidth + gap;
+      }
+      x_offset = 0;
+    }
+
+    // For colored boxes in carryover row
+    for (let i = 0; i < boxesColored; i++) {
+      localSvgArray.push(getRectangle(
+        intialX + x_offset,
+        initialY + y_offset,
+        `Week ${rowsColored * 52 + i + 1}`,
+        true
+      ))
+      x_offset += boxWidth + gap;
+    }
+
+    // Empty boxes in carryover row
+    for (let i = 0; i < boxesBlank; i++) {
+      localSvgArray.push(getRectangle(
+        intialX + x_offset,
+        initialY + y_offset,
+        `Week ${rowsColored * 52 + boxesColored + i + 1}`,
+        false,
+      ));
+      x_offset += boxWidth + gap;
+    }
+
+    if ((rowsColored + 1) % 10 == 0) {
+      y_offset += moat + gap;
+    } else {
+      y_offset += boxWidth + gap;
+    }
+    x_offset = 0;
+
+    // Remaining empty rows
+    let emptyRowNum = 1;
+    for (let i = rowsColored + 1; i < 80; i++) {
+      for (let j = 0; j < 52; j++) {
+        localSvgArray.push(getRectangle(
+          intialX + x_offset,
+          initialY + y_offset,
+          `Week ${(rowsColored + emptyRowNum) * 52 + j + 1}`,
+          false
+        ));
+        x_offset += boxWidth + gap;
+      }
+      if ((i + 1) % 10 == 0) {
+        y_offset += moat + gap;
+      } else {
+        y_offset += boxWidth + gap;
+      }
+      x_offset = 0;
+      emptyRowNum += 1
+    }
+
+    setSvgArray(localSvgArray);
+  }, [weeksLived])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className={`${noto.className} overflow-hidden`}>
+
+        <div className="flex flex-col justify-center items-center mb-4">
+          <h1 className="text-black text-6xl my-5">Memento Mori</h1>
+          <Progress
+            className="w-[40%] h-[10px]"
+            value={weeksLived / (52 * 80) * 100}
+          />
         </div>
+
+        <motion.div
+          className="w-full flex justify-center items-center"
+          initial={{translateX:56/2}}
+          animate={{
+            translateX: (valid) ? [56/2, 0] : ((valid == null) ? [56/2] : [0, 56/2])
+          }}
+        >
+          <Input
+            className="h-[65px] w-[200px] z-20 bg-cream opacity-100 border border-black mr-2 text-xl"
+            placeholder="DD/MM/YYYY"
+            onChange={handleInput}
+          />
+          <motion.div
+            className="border border-black rounded-full z-10"
+            initial={{translateX: -70}}
+            animate={{
+              translateX: (valid) ? [-70, 0] : ((valid == null) ? [-70] : [0, -70])
+            }}
+          >
+            <Button
+              className="bg-inherit h-[56px] w-[56px] hover:bg-inherit hover:opacity-30 active:opacity-20 flex justify-center items-center"
+              onClick={handleClick}
+            >
+              <Skull color="#000000"/>
+            </Button>
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          className="flex justify-center w-full"
+        >
+          <svg 
+            viewbox="0 0 1920 3070"
+            className={`w-[40%]`}
+          >
+            {svgArray && svgArray.map(x => x)}
+          </svg>
+        </motion.div>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </main>  
   );
 }
