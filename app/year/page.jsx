a9fce870c18563b2accbac01b906efc944a150a9
Lalit M <lalitm1004@gmail.com>
2024-12-18 13:19:38 +0530

    fix(year): will rollback to 0% on new year

--- app/year/page.jsx
+++ app/year/page.jsx
@@ -1,40 +1,47 @@
 "use client";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function Year() {
-  
-  const [msPassed, setMsPassed] = useState(null);
-  const [msTotal, setMsTotal] = useState(null);
-  const [percentage, setPercentage] = useState(null);
+  const [msPassed, setMsPassed] = useState(0);
+  const [msTotal, setMsTotal] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
   useEffect(() => {
-    const currentYear = new Date().getFullYear();
+    const updateTimeMetrics = () => {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const startOfYear = new Date(currentYear, 0, 1);
+      const endOfYear = new Date(currentYear + 1, 0, 1);
 
-    const currentDate = new Date();
-    const startOfYear = new Date(currentYear, 0, 1);
-    setMsPassed(currentDate - startOfYear);
+      const msPassed = currentDate - startOfYear;
+      const msTotal = endOfYear - startOfYear;
 
-    const nextYear = currentYear + 1;
-    const endOfYear = new Date(nextYear, 0, 1);
-    setMsTotal(endOfYear - startOfYear);
-  }, [])
+      setMsPassed(msPassed);
+      setMsTotal(msTotal);
+      setPercentage((msPassed / msTotal) * 100);
+    };
 
-  useEffect(() => {
-    const updateIntervalMs = 1;
+    updateTimeMetrics(); // Initial calculation
+
+    const updateIntervalMs = 1; // Update every millisecond
     const intervalId = setInterval(() => {
-      setPercentage((msPassed / msTotal) * 100)
-      setMsPassed(prev => prev + updateIntervalMs);
-    }, updateIntervalMs)
-    return () => clearInterval(intervalId);
-  }, [msPassed, msTotal])
+      updateTimeMetrics(); // Update metrics continuously
+    }, updateIntervalMs);
+
+    return () => clearInterval(intervalId); // Cleanup interval on unmount
+  }, []);
 
   return (
     <main>
-      <div className={`w-screen h-screen flex flex-col md:flex-row justify-center items-center gap-2 text-xl`}>
+      <div
+        className={`w-screen h-screen flex flex-col md:flex-row justify-center items-center gap-2 text-xl`}
+      >
         <p>The year is</p>
-        <p className={`${percentage ? "text-left" : "text-center"} w-[200px]`}>{(percentage) ? percentage : "----------"}</p>
+        <p className={`${percentage ? "text-left" : "text-center"} w-[180px]`}>
+          {percentage ? percentage.toFixed(14): "----------"}
+        </p>
         <p>% over</p>
-      </div>  
+      </div>
     </main>
-  )    
+  );
 }
